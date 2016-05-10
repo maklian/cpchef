387a85383dddae7de29e243630c6dccbb093f5ce
Mark Aklian <mark.aklian@gmail.com>
2016-05-10 11:46:05 +0000

    initial commit repo

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,4 @@
+# precidence - overwrite default attribute if it's needed - default is lowest
+#
+default["apache"]["sites"]["awschef02"] = {"port" => 80, "domain" => "ec2-52-207-221-37.compute-1.amazonaws.com" }
+#default["apache"]["sites"]["awschef01"] = {"port" => 80, "domain" => "ec2-52-91-188-128.compute-1.amazonaws.com" }
